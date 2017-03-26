43752adaa6b39d4f07d51aa9bec8c5b85cb729a2
Yury Mikheev <yumikhee@microsoft.com>
2017-03-25 21:55:17 -0500

    MinMax project init

---
+++ Algo/warmup/minmax/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace minmax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine("Hello World! "+ input);
+        }
+    }
+}
