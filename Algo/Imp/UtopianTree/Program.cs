0eba1567930be98ae51ccb06bf3b60200df752b7
Yury Mikheev <yumikhee@microsoft.com>
2017-03-26 20:05:42 -0500

    Initial state

---
+++ Algo/Imp/UtopianTree/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UtopianTree
+{
+    class Solution {
+
+    static void Main(String[] args) {
+        int t = Convert.ToInt32(Console.ReadLine());
+        for(int a0 = 0; a0 < t; a0++){
+            int n = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
+}
